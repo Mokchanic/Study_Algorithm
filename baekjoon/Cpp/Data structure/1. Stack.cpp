977b37a3516272ad598183ddff76c7a05d593aa1
MokJinWoo_Player <63028144+Mokchanic@users.noreply.github.com>
2022-07-01 11:11:45 +0900

    update
    _stack_cpp

--- baekjoon/Cpp/Data structure/1. Stack.cpp
+++ baekjoon/Cpp/Data structure/1. Stack.cpp
@@ -1,25 +1,73 @@
 #include <iostream>
 #include <typeinfo>
-#include <string>
+#include <vector>
 
 using namespace std;
 
-#define SIZE = 100000;
-
 int main()
 {
-
     int num_value;
+    vector<int> stack;
+    string input_value;
 
     //input_repeat
     cin >> num_value;
 
     for (int i = 0; i < num_value; i++)
         {
-            string input_value;
             cin >> input_value;
-            cout << input_value << endl;
-            cout << sizeof(input_value) << endl;
+            
+            if (input_value == "push")
+            {
+                int number;
+                cin >> number;
+                stack.push_back(number);
+            }
+
+            else if (input_value == "pop")
+            {
+                if (stack.size() > 0)
+                {
+                    cout << stack.back() << endl;
+                    stack.pop_back();
+                }                    
+
+                else if (stack.size() == 0)
+                {
+                    cout << "-1" << endl;
+                }   
+            }
+
+            else if (input_value == "size")
+            {
+                cout << stack.size() << endl;
+            }
+
+            else if (input_value == "empty")
+            {
+                if (stack.size() > 0)
+                {
+                    cout << "0" << endl;
+                }                    
+
+                else if (stack.size() == 0)
+                {
+                    cout << "1" << endl;
+                }   
+            }
+
+            else if (input_value == "top")
+            {                
+                if (stack.size() > 0)
+                {
+                    cout << stack.back() << endl;
+                }                    
+
+                else if (stack.size() == 0)
+                {
+                    cout << "-1" << endl;
+                }   
+            }
         }
 
     return 0;
--- baekjoon/Cpp/test.cpp
+++ baekjoon/Cpp/test.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
 
 using namespace std;
 
+template <size_t N>
+void pri(const std::array<int, N>& arr2)
+{
+	for (auto element : arr2)
+		cout << element << endl;
+}
+
 int main()
 {
-    cout << "Hello, World!" << endl;
-    
-    return 0;
+	array<int, 5> arr2 = {1,2,3,4};
+
+	//cout << arr2.begin() << " " << arr2.end();
+	for (auto it = arr2.begin(); it != arr2.end(); it++)
+	{
+		auto element = (*it);
+		cout << element << endl;
+	}
+
+	return 0;
 }
